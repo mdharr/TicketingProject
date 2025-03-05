5c2f064eb6fbedb178cf4abf4df603dc65f64024
Michael Harrington <mdharr7@gmail.com>
2025-03-05 23:24:00 +0800

    Implement Circuit Breaker pattern to monitor and regulate requests to booking service and prevent overloading server when failing. This works based on switching between closed, open, and half-open states. The requests coming from the client, through the api gateway, have different limits based on the state of the Resilience4j (circuit breaker). Use Open API to generate Swagger documentation for booking and inventory services.

---
+++ bookingservice/src/main/java/com/mdharr/bookingservice/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.mdharr.bookingservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI inventoryServiceApi() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Booking Service API")
+                        .description("Booking Service API for Ticketing Application with Microservices Architecture")
+                        .version("v1.0.0"));
+    }
+}
